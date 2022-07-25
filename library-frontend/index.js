d16dd3a576105486f8e588d24c0885b368b2fc94
Nick <nelom3@gmail.com>
2022-07-25 17:02:08 -0400

    defines Genre class and renderGenres function

--- library-frontend/index.js
+++ library-frontend/index.js
@@ -3,7 +3,37 @@ const url = "http://127.0.0.1:3000"
 function getGenres() {
   fetch(`${url}/genres`)
   .then(resp => resp.json())
-  .then(json => console.log(json))
+  .then(json => {
+    json.forEach(obj => {
+      new Genre(obj.name)
+    })
+    renderGenres()
+    console.log(Genre.genres)
+  })
 }
+
+function renderGenres() {
+  const genreContainer = document.getElementById("genre-container")
+  const ul = document.createElement("ul")
+  genreContainer.appendChild(ul)
+  // document.appendChild(ul)
+  Genre.genres.forEach(genre => {
+    const li = document.createElement("li")
+    li.innerText = genre.name
+    ul.appendChild(li)
+  })
+}
+
+class Genre {
+
+  static genres = []
+
+  constructor(name) {
+    this.name = name
+    Genre.genres.push(this)
+  }
+}
+
+
 
 getGenres()
