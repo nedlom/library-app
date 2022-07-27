c9778652a7340cab13d057d3b544f589c0f6638b
Nick <nelom3@gmail.com>
2022-07-27 18:48:52 -0400

    create new genre form and newGenre function refactor

--- library-frontend/src/genre.js
+++ library-frontend/src/genre.js
@@ -1,6 +1,5 @@
 class Genre {
 
-  static genreForm = document.getElementById("form-container")
   static genres = []
 
   constructor(id, name) {
@@ -23,14 +22,57 @@ class Genre {
   }
 
   static createForm() {
-    console.log(this.genreForm)
+    const genreForm = document.getElementById("form-container")
+
+    const form = document.createElement('form')
+    form.id = "genre-form"
+
+    const input = document.createElement('input')
+    input.type = "text"
+    input.id = "genre-name"
+
+    const submit = document.createElement('input')
+    submit.type = "submit"
+    submit.id = "submit"
+    submit.value = "Create Genre"
+
+    form.appendChild(input)
+    form.appendChild(submit)
+    genreForm.appendChild(form)
+
+    form.addEventListener("submit", this.newGenre)
+  }
+
+  static newGenre() {
+  
+    event.preventDefault()
+    // console.log(this)
+
+    fetch(`${url}/genres`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: document.getElementById('genre-name').value
+      })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+      const genre = new Genre(json.id, json.name)
+      genre.renderGenre()
+      // console.log(this)
+    })
+
+    // this.reset()
   }
 
   renderGenre() {
     const genreList = document.getElementById("genre-list")
 
     const delBttn = document.createElement("button")
-    delBttn.innerText = "Delete"
+    delBttn.innerText = "pretend this is a trashcan"
     delBttn.className = "delete"
     delBttn.dataset.id = this.id
 
@@ -58,5 +100,4 @@ class Genre {
   this.parentElement.remove()
 }
     
-
 }
