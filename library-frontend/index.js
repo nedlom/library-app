d9eceb16df06175a1a9ad22c5952ff50b8618af8
Nick <nelom3@gmail.com>
2022-07-28 18:38:45 -0400

    adds event listener to genre form

--- library-frontend/index.js
+++ library-frontend/index.js
@@ -1,5 +1,5 @@
 const url = "http://127.0.0.1:3000"
+const genreForm = document.getElementById("genre-form")
 
-
+genreForm.addEventListener("submit", Genre.newGenre)
 Genre.getGenres()
-Genre.createForm()
