bc1348affe1f66096209044f075bd362d3e38821
Nick <nelom3@gmail.com>
2022-07-28 18:39:33 -0400

    refactor

--- library-frontend/src/genre.js
+++ library-frontend/src/genre.js
@@ -2,9 +2,10 @@ class Genre {
 
   static genres = []
 
-  constructor(id, name) {
+  constructor(id, name, books) {
     this.id = id
     this.name = name
+    this.books = books
     Genre.genres.push(this)
   }
 
@@ -12,42 +13,45 @@ class Genre {
     fetch(`${url}/genres`)
     .then(resp => resp.json())
     .then(json => {
-      json.forEach(obj => this.createGenre(obj))
+      json.forEach(obj => {
+        const genre = new Genre(obj.id, obj.name, obj.books)
+      })
+      this.renderGenres()  
     })
   }
 
-  static createGenre(obj) {
-    const genre = new Genre(obj.id, obj.name)
-    genre.renderGenre()
+  static renderGenres() {
+    this.genres.forEach(genre => genre.renderGenre())
   }
 
-  static createForm() {
-    const genreForm = document.getElementById("form-container")
+  renderGenre() {
+    const genreList = document.getElementById("genre-list")
 
-    const form = document.createElement('form')
-    form.id = "genre-form"
+    const delBttn = document.createElement("button")
+    delBttn.innerText = "pretend this is a trashcan"
+    delBttn.className = "delete"
+    delBttn.dataset.id = this.id
 
-    const input = document.createElement('input')
-    input.type = "text"
-    input.id = "genre-name"
+    const li = document.createElement("li")
+    li.innerText = this.name
 
-    const submit = document.createElement('input')
-    submit.type = "submit"
-    submit.id = "submit"
-    submit.value = "Create Genre"
+    // if (this.books !== []) {
+    //   let x = this.books[0].description
+    //   let p = document.createElement("p")
+    //   p.innerText = x
+    //   li.append(p)
+    //   // debugger
+    // }
 
-    form.appendChild(input)
-    form.appendChild(submit)
-    genreForm.appendChild(form)
+    li.appendChild(delBttn)
 
-    form.addEventListener("submit", this.newGenre)
+    delBttn.addEventListener("click", this.deleteGenre)
+
+    genreList.appendChild(li)
   }
 
   static newGenre() {
-  
     event.preventDefault()
-    // console.log(this)
-
     fetch(`${url}/genres`, {
       method: "POST",
       headers: {
@@ -62,27 +66,9 @@ class Genre {
     .then(json => {
       const genre = new Genre(json.id, json.name)
       genre.renderGenre()
-      // console.log(this)
+     
     })
-
-    // this.reset()
-  }
-
-  renderGenre() {
-    const genreList = document.getElementById("genre-list")
-
-    const delBttn = document.createElement("button")
-    delBttn.innerText = "pretend this is a trashcan"
-    delBttn.className = "delete"
-    delBttn.dataset.id = this.id
-
-    const li = document.createElement("li")
-    li.innerText = this.name
-    li.appendChild(delBttn)
-
-    delBttn.addEventListener("click", this.deleteGenre)
-
-    genreList.appendChild(li)
+    this.reset()
   }
 
   deleteGenre() {
@@ -93,11 +79,9 @@ class Genre {
       method: "DELETE",
       headers: {
           "Content-Type": "application/json"
-      }
-  })
-
-  console.log(this.parentElement)
-  this.parentElement.remove()
-}
+        }
+    })
+    this.parentElement.remove()
+  }  
     
 }
