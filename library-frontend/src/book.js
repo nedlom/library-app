5f9e148a7893459b89a7d652f0578708651ee4f2
Nick <nelom3@gmail.com>
2022-07-29 16:47:27 -0400

    defines book class with constructor and createBookDiv function to build book div tag

--- library-frontend/src/book.js
+++ library-frontend/src/book.js
@@ -1,8 +1,39 @@
 class Book {
 
-  constructor() {
+  constructor(title, author, description) {
     this.title = title
     this.author = author
     this.description = description
   }
+
+  createBookDiv() {
+    const div = document.createElement("div")
+
+    const h3 = document.createElement("h3")
+    const p = document.createElement("p")
+
+    h3.innerText = this.title
+    p.innerText = `by ${this.author} \n\n ${this.description}`
+
+    
+    div.appendChild(h3)
+    div.appendChild(p)
+
+    return div
+  }
+
+  deleteGenre() {
+    event.preventDefault()
+    const id = this.dataset.id
+
+    fetch(`${url}/genres/${id}`, {
+      method: "DELETE",
+      headers: {
+          "Content-Type": "application/json"
+        }
+    })
+    .catch(() => console.log("error"))
+    this.parentElement.remove()
+  }  
+
 }
