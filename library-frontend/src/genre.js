fc17e72e6e0eff8c85dcd547b66038ab5553862e
Nick <nelom3@gmail.com>
2022-07-30 14:33:35 -0400

    Click event to render new book form

--- library-frontend/src/genre.js
+++ library-frontend/src/genre.js
@@ -14,8 +14,6 @@ class Genre {
     .then(resp => resp.json())
     .then(json => {
       json.forEach(obj => {
-
-        
         const genre = new Genre(obj.id, obj.name, obj.books)
       })
       this.renderGenres()  
@@ -39,7 +37,23 @@ class Genre {
     delBttn.dataset.id = this.id
 
     const li = document.createElement("li")
-    li.innerText = this.name
+    const div = document.createElement("div")
+    div.id = "li-title"
+    div.innerText = this.name
+    li.appendChild(div)
+    // li.innerText = this.name
+
+    const bookDiv = document.createElement("div")
+    bookDiv.innerText = "BOOK DIV"
+    bookDiv.id = "myDiv"
+    bookDiv.style.display = "none"
+
+    const bookBttn = document.createElement("button")
+    bookBttn.innerText = "Add Book"
+    bookBttn.addEventListener("click", this.myFunction)
+    // const bookForm = Book.renderBookForm()
+    li.appendChild(bookBttn)
+    li.appendChild(bookDiv)
 
     if (this.books !== []) {
       this.books.forEach(obj => {
@@ -47,13 +61,39 @@ class Genre {
         const bookDiv = book.createBookDiv()
         li.appendChild(bookDiv)
       })
-  }
+    }
 
     li.appendChild(delBttn)
 
     delBttn.addEventListener("click", this.deleteGenre)
 
     genreList.appendChild(li)
+  }
+
+  myFunction() {
+
+    const myDiv = document.getElementById("myDiv")
+
+    // debugger
+    if (myDiv.style.display === "none") {
+      myDiv.style.display = "block";
+    } else {
+      myDiv.style.display = "none";
+    }
+
+    myDiv.innerHTML = `<div id="book-form-container">
+    <form id="book-form">
+      <label>Title</label><br>
+      <input type="text" id="title"><br>
+      <label>Author</label><br>
+      <input type="text" id="author"><br>
+      <label>Description</label><br>
+      <textarea type="text" id="description"></textarea><br>
+      <input type="submit" value="Add Book">
+    </form>
+  </div>`
+
+  debugger
   }
 
   static newGenre() {
