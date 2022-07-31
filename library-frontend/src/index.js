23ef92aa25366b043f7036394aeb12602a6947e7
Nick <nelom3@gmail.com>
2022-07-31 14:26:02 -0400

    GenreAdapter entry point to application

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -1,5 +1,11 @@
-const url = "http://127.0.0.1:3000"
-const genreForm = document.getElementById("genre-form")
+// const url = "http://127.0.0.1:3000"
+// const genreForm = document.getElementById("genre-form")
 
-genreForm.addEventListener("submit", Genre.newGenre)
-Genre.getGenres()
+// genreForm.addEventListener("submit", Genre.newGenre)
+// Genre.getGenres()
+
+// const genreForm = document.getElementById("genre-form")
+const genreAdapter = new GenreAdapter()
+// genreForm.addEventListener("submit", genreAdapter.newGenre.bind(genreAdapter))
+genreAdapter.fetchGenres()
+
