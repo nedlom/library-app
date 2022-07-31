c041c8c22fbe176ee53b5616db249f6328d9af9a
Nick <nelom3@gmail.com>
2022-07-31 15:20:59 -0400

    Refactors renderGenre method into multiple component methods

--- library-frontend/src/components/genre.js
+++ library-frontend/src/components/genre.js
@@ -23,47 +23,27 @@ class Genre {
     }
   }
 
-  // static getGenres() {
-  //   fetch(`${url}/genres`)
-  //   .then(resp => resp.json())
-  //   .then(json => {
-  //     json.forEach(obj => {
-  //       const genre = new Genre(obj.id, obj.name, obj.books)
-  //     })
-  //     this.renderGenres()  
-  //   })
-  // }
-
-  // static getBooks() {
-  //   this.genres
-  // }
-
-  // static renderGenres() {
-  //   this.all.forEach(genre => genre.renderGenre())
-  // }
-
   static renderGenres() {
-    this.all.forEach(genre => genre.createGenreDiv())
+    this.all.forEach(genre => genre.renderGenre())
   }
 
   renderGenre() {
-
+    const genreContainer = document.getElementById("genre-container")
+    const genreDiv = this.createGenreDiv()
+    genreContainer.append(genreDiv)
   }
 
   createGenreDiv() {
     const div = document.createElement("div")
     div.className = "genre-div"
+    div.id = this.id
 
-    div.append(this.nameAndDelete(), this.bookTags())
+    div.append(this.nameAndDelete())
 
-    const genreContainer = document.getElementById("genre-container")
-    genreContainer.append(div)
-
-    // const nameAndDelete = document.createElement("div")
-    // nameAndDelete.className = "genre-name-delete"
-
-    // const genreBooks = document.createElement("div")
-    // genreBooks.className = "genre-books"
+    const bookDivs = this.bookTags()
+    bookDivs.forEach(book => div.append(book))
+    
+    return div
   }
 
   nameAndDelete() {
@@ -84,104 +64,85 @@ class Genre {
     const button = document.createElement("button")
     button.className = "child"
     button.innerHTML = `Delete ${this.name}`
+    button.addEventListener("click", genreAdapter.deleteGenre.bind(this))
     return button
   }
 
-  booksTags() {
-    
+  bookTags() {
+    return this.books.map(book => book.createBookDiv())
   }
 
-
-
-
-    // const div = document.createElement("div")
-    // div.id = this.id
-
-    // const h3 = document.createElement("h3")
-    // h3.innerHTML = this.name
+  // renderGenre2() {
+  //   const genreList = document.getElementById("genre-list")
 
     // const delBttn = document.createElement("button")
-    // delBttn.innerHTML = `Delete ${this.name}`
+    // delBttn.innerText = "pretend this is a trashcan"
     // delBttn.className = "delete"
+    // delBttn.dataset.id = this.id
 
-    // div.append(h3, delBttn)
+    // const li = document.createElement("li")
+    // const div = document.createElement("div")
+    // div.id = "li-title"
+    // div.innerText = this.name
+    // li.appendChild(div)
+    // li.id = this.id
+    // // li.innerText = this.name
 
-    // const genreContainer = document.getElementById("genre-container")
-    // genreContainer.append(div)
+    // const bookDiv = document.createElement("div")
+    // bookDiv.innerText = "BOOK DIV"
+    // bookDiv.id = "myDiv"
+    // bookDiv.style.display = "none"
 
-  // }
-
-  renderGenre() {
-    const genreList = document.getElementById("genre-list")
-
-    const delBttn = document.createElement("button")
-    delBttn.innerText = "pretend this is a trashcan"
-    delBttn.className = "delete"
-    delBttn.dataset.id = this.id
-
-    const li = document.createElement("li")
-    const div = document.createElement("div")
-    div.id = "li-title"
-    div.innerText = this.name
-    li.appendChild(div)
-    li.id = this.id
-    // li.innerText = this.name
-
-    const bookDiv = document.createElement("div")
-    bookDiv.innerText = "BOOK DIV"
-    bookDiv.id = "myDiv"
-    bookDiv.style.display = "none"
-
-    const bookBttn = document.createElement("button")
-    bookBttn.innerText = "Add Book"
-    bookBttn.addEventListener("click", this.myFunction)
+    // const bookBttn = document.createElement("button")
+    // bookBttn.innerText = "Add Book"
+    // bookBttn.addEventListener("click", this.myFunction)
     // const bookForm = Book.renderBookForm()
-    li.appendChild(bookBttn)
-    li.appendChild(bookDiv)
+    // li.appendChild(bookBttn)
+    // li.appendChild(bookDiv)
 
-    if (this.books !== []) {
-      this.books.forEach(obj => {
+    // if (this.books !== []) {
+    //   this.books.forEach(obj => {
 
-        const book = new Book(obj.id, obj.title, obj.author, obj.description)
-        const bookDiv = book.createBookDiv()
-        li.appendChild(bookDiv)
-      })
-    }
+    //     const book = new Book(obj.id, obj.title, obj.author, obj.description)
+    //     const bookDiv = book.createBookDiv()
+    //     li.appendChild(bookDiv)
+    //   })
+    // }
 
-    li.appendChild(delBttn)
+    // li.appendChild(delBttn)
 
     // delBttn.addEventListener("click", this.deleteGenre)
 
-    delBttn.addEventListener("click", genreAdapter.deleteGenre.bind(this))
+  //   delBttn.addEventListener("click", genreAdapter.deleteGenre.bind(this))
 
-    genreList.appendChild(li)
-  }
+  //   genreList.appendChild(li)
+  // }
 
-  myFunction() {
+  // myFunction() {
 
-    const myDiv = document.getElementById("myDiv")
+  //   const myDiv = document.getElementById("myDiv")
 
     // debugger
-    if (myDiv.style.display === "none") {
-      myDiv.style.display = "block";
-    } else {
-      myDiv.style.display = "none";
-    }
+  //   if (myDiv.style.display === "none") {
+  //     myDiv.style.display = "block";
+  //   } else {
+  //     myDiv.style.display = "none";
+  //   }
 
-    myDiv.innerHTML = `<div id="book-form-container">
-    <form id="book-form">
-      <label>Title</label><br>
-      <input type="text" id="title"><br>
-      <label>Author</label><br>
-      <input type="text" id="author"><br>
-      <label>Description</label><br>
-      <textarea type="text" id="description"></textarea><br>
-      <input type="submit" value="Add Book">
-    </form>
-  </div>`
+  //   myDiv.innerHTML = `<div id="book-form-container">
+  //   <form id="book-form">
+  //     <label>Title</label><br>
+  //     <input type="text" id="title"><br>
+  //     <label>Author</label><br>
+  //     <input type="text" id="author"><br>
+  //     <label>Description</label><br>
+  //     <textarea type="text" id="description"></textarea><br>
+  //     <input type="submit" value="Add Book">
+  //   </form>
+  // </div>`
 
-  debugger
-  }
+  // debugger
+  // }
 
   // static newGenre() {
   //   event.preventDefault()
