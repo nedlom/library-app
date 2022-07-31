70b9bf5c6e8a37aec4c4ac4b15c97e13bc1b051f
Nick <nelom3@gmail.com>
2022-07-31 14:24:29 -0400

    Refactor to accommodate addition of GenreAdapter

--- library-frontend/src/components/genre.js
+++ library-frontend/src/components/genre.js
@@ -1,32 +1,115 @@
 class Genre {
 
-  static genres = []
+  static all = []
 
   constructor(id, name, books) {
     this.id = id
     this.name = name
-    this.books = books
-    Genre.genres.push(this)
+    this.books = this.getBooks(books)
+    Genre.all.push(this)
   }
 
-  static getGenres() {
-    fetch(`${url}/genres`)
-    .then(resp => resp.json())
-    .then(json => {
-      json.forEach(obj => {
-        const genre = new Genre(obj.id, obj.name, obj.books)
-      })
-      this.renderGenres()  
-    })
+  getBooks(books) {
+    if (books) {
+      return books.map(book => new Book(
+        book.id, 
+        book.title, 
+        book.author, 
+        book.description, 
+        book.genre_id
+      ))
+    } else {
+        return []
+    }
   }
 
-  static getBooks() {
-    this.genres
-  }
+  // static getGenres() {
+  //   fetch(`${url}/genres`)
+  //   .then(resp => resp.json())
+  //   .then(json => {
+  //     json.forEach(obj => {
+  //       const genre = new Genre(obj.id, obj.name, obj.books)
+  //     })
+  //     this.renderGenres()  
+  //   })
+  // }
+
+  // static getBooks() {
+  //   this.genres
+  // }
+
+  // static renderGenres() {
+  //   this.all.forEach(genre => genre.renderGenre())
+  // }
 
   static renderGenres() {
-    this.genres.forEach(genre => genre.renderGenre())
+    this.all.forEach(genre => genre.createGenreDiv())
   }
+
+  renderGenre() {
+
+  }
+
+  createGenreDiv() {
+    const div = document.createElement("div")
+    div.className = "genre-div"
+
+    div.append(this.nameAndDelete(), this.bookTags())
+
+    const genreContainer = document.getElementById("genre-container")
+    genreContainer.append(div)
+
+    // const nameAndDelete = document.createElement("div")
+    // nameAndDelete.className = "genre-name-delete"
+
+    // const genreBooks = document.createElement("div")
+    // genreBooks.className = "genre-books"
+  }
+
+  nameAndDelete() {
+    const div = document.createElement("div")
+    div.className = "parent"
+    div.append(this.nameTag(), this.deleteButton())
+    return div
+  }
+
+  nameTag() {
+    const div = document.createElement("div")
+    div.className = "child"
+    div.innerHTML = this.name
+    return div
+  }
+
+  deleteButton() {
+    const button = document.createElement("button")
+    button.className = "child"
+    button.innerHTML = `Delete ${this.name}`
+    return button
+  }
+
+  booksTags() {
+    
+  }
+
+
+
+
+    // const div = document.createElement("div")
+    // div.id = this.id
+
+    // const h3 = document.createElement("h3")
+    // h3.innerHTML = this.name
+
+    // const delBttn = document.createElement("button")
+    // delBttn.innerHTML = `Delete ${this.name}`
+    // delBttn.className = "delete"
+
+    // div.append(h3, delBttn)
+
+    // const genreContainer = document.getElementById("genre-container")
+    // genreContainer.append(div)
+
+  // }
 
   renderGenre() {
     const genreList = document.getElementById("genre-list")
@@ -41,6 +124,7 @@ class Genre {
     div.id = "li-title"
     div.innerText = this.name
     li.appendChild(div)
+    li.id = this.id
     // li.innerText = this.name
 
     const bookDiv = document.createElement("div")
@@ -57,6 +141,7 @@ class Genre {
 
     if (this.books !== []) {
       this.books.forEach(obj => {
+
         const book = new Book(obj.id, obj.title, obj.author, obj.description)
         const bookDiv = book.createBookDiv()
         li.appendChild(bookDiv)
@@ -65,7 +150,9 @@ class Genre {
 
     li.appendChild(delBttn)
 
-    delBttn.addEventListener("click", this.deleteGenre)
+    // delBttn.addEventListener("click", this.deleteGenre)
+
+    delBttn.addEventListener("click", genreAdapter.deleteGenre.bind(this))
 
     genreList.appendChild(li)
   }
@@ -96,39 +183,39 @@ class Genre {
   debugger
   }
 
-  static newGenre() {
-    event.preventDefault()
-    fetch(`${url}/genres`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        name: document.getElementById('genre-name').value
-      })
-    })
-    .then(resp => resp.json())
-    .then(json => {
-      const genre = new Genre(json.id, json.name, json.books)
-      genre.renderGenre()
+  // static newGenre() {
+  //   event.preventDefault()
+  //   fetch(`${url}/genres`, {
+  //     method: "POST",
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       name: document.getElementById('genre-name').value
+  //     })
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(json => {
+  //     const genre = new Genre(json.id, json.name, json.books)
+  //     genre.renderGenre()
      
-    })
-    this.reset()
-  }
+  //   })
+  //   this.reset()
+  // }
 
-  deleteGenre() {
-    event.preventDefault()
-    const id = this.dataset.id
+  // deleteGenre() {
+  //   event.preventDefault()
+  //   const id = this.dataset.id
 
-    fetch(`${url}/genres/${id}`, {
-      method: "DELETE",
-      headers: {
-          "Content-Type": "application/json"
-        }
-    })
-    .catch(() => console.log("error"))
-    this.parentElement.remove()
-  }  
+  //   fetch(`${url}/genres/${id}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //         "Content-Type": "application/json"
+  //       }
+  //   })
+  //   .catch(() => console.log("error"))
+  //   this.parentElement.remove()
+  // }  
     
 }
