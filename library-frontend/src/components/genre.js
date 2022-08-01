6c50f62d8b9c75f1ffb71758a5f2f6d1adfa9d2d
Nick <nelom3@gmail.com>
2022-08-01 17:33:58 -0400

    Remove unused code and refactor

--- library-frontend/src/components/genre.js
+++ library-frontend/src/components/genre.js
@@ -38,7 +38,7 @@ class Genre {
     div.className = "genre-div"
     div.id = this.id
 
-    div.append(this.nameAndDelete(), this.addBookButton())
+    div.append(this.nameAndDeleteDiv(), this.bookBtnAndFormDiv())
 
     const bookDivs = this.bookTags()
     bookDivs.forEach(book => div.append(book))
@@ -46,21 +46,21 @@ class Genre {
     return div
   }
 
-  nameAndDelete() {
+  nameAndDeleteDiv() {
     const div = document.createElement("div")
     div.className = "parent"
-    div.append(this.nameTag(), this.deleteButton())
+    div.append(this.nameDiv(), this.deleteDiv())
     return div
   }
 
-  nameTag() {
+  nameDiv() {
     const div = document.createElement("div")
     div.className = "child"
     div.innerHTML = this.name
     return div
   }
 
-  deleteButton() {
+  deleteDiv() {
     const button = document.createElement("button")
     button.className = "child"
     button.innerHTML = `Delete ${this.name}`
@@ -68,166 +68,32 @@ class Genre {
     return button
   }
 
-  addBookButton() {
-    const buttonDiv = document.createElement("div")
-    // buttonDiv.id = "add-book-button-container"
-    buttonDiv.id = `genre-id-${this.id}`
-    buttonDiv.className = "add-book-to-genre"
+  bookBtnAndFormDiv() {
+    const buttonFormDiv = document.createElement("div")
+    buttonFormDiv.className = "book-button-form"
+    
+    buttonFormDiv.append(this.bookButton(), this.bookForm())
+    return buttonFormDiv
+  }
 
+  bookButton() {
     const button = document.createElement("button")
     button.id = `button${this.id}`
     button.innerHTML = `Add Book To ${this.name}` 
-    // button.className = "add-book-to-genre"
+    button.addEventListener("click", this.hideBookBtdDisplayForm)
+    return button
+  }
 
-    buttonDiv.append(button)
+  bookForm() {
+    return Book.createBookForm(this.id)
+  }
 
-    button.addEventListener("click", this.removeBtnGetForm.bind(this))
-    
-    return buttonDiv
+  hideBookBtdDisplayForm() {
+    this.parentElement.children[0].className = "no-display"
+    this.parentElement.children[1].className = ""
   }
 
   bookTags() {
     return this.books.map(book => book.createBookDiv())
   }
-
-  removeBtnGetForm() {
-    this.hideNewBookButton()
-    this.getBookForm()
-    // return Book.renderBookForm()
-  }
-
-  hideNewBookButton() {
-    // debugger
-
-    // const id = this.id
-    const div = document.getElementById(`button${this.id}`)
-
-    // const div = document.getElementById("add-book-to-genre")
-    if (div.style.display === "none") {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
-  }
-
-  getBookForm() {
-    // debugger
-    
-    // const div = document.getElementById(this.id)
-    
-    const div = document.getElementById(`genre-id-${this.id}`)
-
-    // const bttnContainer = div.getElementById("add-book-button-container")
-    // const div = document.getElementById("add-book-button-container")
-    div.append(Book.createBookForm(this.id))
-    // bttnContainer.append(Book.createBookForm())
-  }
-
-  // renderGenre2() {
-  //   const genreList = document.getElementById("genre-list")
-
-    // const delBttn = document.createElement("button")
-    // delBttn.innerText = "pretend this is a trashcan"
-    // delBttn.className = "delete"
-    // delBttn.dataset.id = this.id
-
-    // const li = document.createElement("li")
-    // const div = document.createElement("div")
-    // div.id = "li-title"
-    // div.innerText = this.name
-    // li.appendChild(div)
-    // li.id = this.id
-    // // li.innerText = this.name
-
-    // const bookDiv = document.createElement("div")
-    // bookDiv.innerText = "BOOK DIV"
-    // bookDiv.id = "myDiv"
-    // bookDiv.style.display = "none"
-
-    // const bookBttn = document.createElement("button")
-    // bookBttn.innerText = "Add Book"
-    // bookBttn.addEventListener("click", this.myFunction)
-    // const bookForm = Book.renderBookForm()
-    // li.appendChild(bookBttn)
-    // li.appendChild(bookDiv)
-
-    // if (this.books !== []) {
-    //   this.books.forEach(obj => {
-
-    //     const book = new Book(obj.id, obj.title, obj.author, obj.description)
-    //     const bookDiv = book.createBookDiv()
-    //     li.appendChild(bookDiv)
-    //   })
-    // }
-
-    // li.appendChild(delBttn)
-
-    // delBttn.addEventListener("click", this.deleteGenre)
-
-  //   delBttn.addEventListener("click", genreAdapter.deleteGenre.bind(this))
-
-  //   genreList.appendChild(li)
-  // }
-
-  // myFunction() {
-
-  //   const myDiv = document.getElementById("myDiv")
-
-    // debugger
-  //   if (myDiv.style.display === "none") {
-  //     myDiv.style.display = "block";
-  //   } else {
-  //     myDiv.style.display = "none";
-  //   }
-
-  //   myDiv.innerHTML = `<div id="book-form-container">
-  //   <form id="book-form">
-  //     <label>Title</label><br>
-  //     <input type="text" id="title"><br>
-  //     <label>Author</label><br>
-  //     <input type="text" id="author"><br>
-  //     <label>Description</label><br>
-  //     <textarea type="text" id="description"></textarea><br>
-  //     <input type="submit" value="Add Book">
-  //   </form>
-  // </div>`
-
-  // debugger
-  // }
-
-  // static newGenre() {
-  //   event.preventDefault()
-  //   fetch(`${url}/genres`, {
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       name: document.getElementById('genre-name').value
-  //     })
-  //   })
-  //   .then(resp => resp.json())
-  //   .then(json => {
-  //     const genre = new Genre(json.id, json.name, json.books)
-  //     genre.renderGenre()
-     
-  //   })
-  //   this.reset()
-  // }
-
-  // deleteGenre() {
-  //   event.preventDefault()
-  //   const id = this.dataset.id
-
-  //   fetch(`${url}/genres/${id}`, {
-  //     method: "DELETE",
-  //     headers: {
-  //         "Content-Type": "application/json"
-  //       }
-  //   })
-  //   .catch(() => console.log("error"))
-  //   this.parentElement.remove()
-  // }  
-    
 }
