352003ff5f1145f90044349a47c6bce9ec583df4
Nick <nelom3@gmail.com>
2022-08-02 08:27:40 -0400

    Deletes unused code

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -1,11 +1,2 @@
-// const url = "http://127.0.0.1:3000"
-// const genreForm = document.getElementById("genre-form")
-
-// genreForm.addEventListener("submit", Genre.newGenre)
-// Genre.getGenres()
-
-// const genreForm = document.getElementById("genre-form")
-const genreAdapter = new GenreAdapter()
-// genreForm.addEventListener("submit", genreAdapter.newGenre.bind(genreAdapter))
-genreAdapter.fetchGenres()
-
+GenreAdapter.fetchGenres()
+Genre.getForm()
