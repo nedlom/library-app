ef4663312acf0518365eb0ef4abf1b2c9969c9c7
Nick <nelom3@gmail.com>
2022-08-04 01:51:53 -0400

    Refactor to add 'genre empty' div to genre's book container when genre has no books

--- library-frontend/src/adapters/bookAdapter.js
+++ library-frontend/src/adapters/bookAdapter.js
@@ -4,8 +4,9 @@ class BookAdapter {
 
   static newBook() {
     event.preventDefault()
+ 
     const id = this.parentElement.dataset.id
-   
+    
     const book = {
       title: document.getElementById(`title-${id}`).value,
       author: document.getElementById(`author-${id}`).value, 
@@ -26,22 +27,50 @@ class BookAdapter {
     .then(resp => resp.json())
     .then(json => {
       const book = new Book(json.id, json.title, json.author, json.description, json.genre_id)
-      book.renderBook()
+      Genre.findById(book.genre_id).books.push(book)
+      const bookGenre = document.getElementById(id).getElementsByClassName("books")[0]
+      bookGenre.append(book.createBookDiv())
+      
+      if (bookGenre.children[0].className === 'book-div no-books') {
+        bookGenre.children[0].remove()
+      }
     })
-    
+   
     this.reset()
-    this.parentElement.className = "no-display"
+    const bookForm = document.getElementById(`book-form-div-container-${id}`)
+    bookForm.className = "no-display"
   }
 
   static deleteBook() {
     event.preventDefault()
-      fetch(`${BookAdapter.url}/${this.id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-          }
-      })
 
-      document.getElementById(`book-${this.id}`).remove()
+    fetch(`${BookAdapter.url}/${this.id}`, {
+      method: "DELETE",
+      headers: {
+          "Content-Type": "application/json"
+        } 
+    }).then(() => { 
+      
+      const genreElement = document.getElementById(this.genre_id)
+
+      const booksElement = genreElement.getElementsByClassName("books")[0]
+      booksElement.innerHTML = ""
+
+      const genre = this.genre()
+      const genreBooks = this.genre().books
+      const updateGenreBooks = genreBooks.filter(book => book !== this)
+
+      genre.books = updateGenreBooks
+
+      if (genre.hasBooks()) {
+        const bookDivs = genre.bookDivs()
+        bookDivs.forEach(book => booksElement.append(book))
+      } else {
+        const noBooks = document.createElement("div")
+        noBooks.className = "book-div no-books"
+        noBooks.innerHTML = `${genre.name} genre empty.`
+        booksElement.append(noBooks)
+      }
+    }) 
   }  
 }
