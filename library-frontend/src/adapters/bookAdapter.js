60a1becb96b19ef697ed793d08d18e5a25465330
Nick <nelom3@gmail.com>
2022-08-05 10:09:29 -0400

    Adds appendChildren method to update genre div

--- library-frontend/src/adapters/bookAdapter.js
+++ library-frontend/src/adapters/bookAdapter.js
@@ -4,15 +4,13 @@ class BookAdapter {
 
   static newBook() {
     event.preventDefault()
- 
-    const id = this.parentElement.dataset.id
-    
-    const book = {
-      title: document.getElementById(`title-${id}`).value,
-      author: document.getElementById(`author-${id}`).value, 
-      description: document.getElementById(`description-${id}`).value, 
-      genre_id: id
-    }
+
+    const formInputs = this.querySelectorAll(".input")
+    const book = {}
+    formInputs.forEach(input => {
+      book[input.id] = input.value
+    })
+    book["genre_id"] = this.parentElement.dataset.id
     
     fetch(BookAdapter.url, {
       method: "POST",
@@ -27,50 +25,32 @@ class BookAdapter {
     .then(resp => resp.json())
     .then(json => {
       const book = new Book(json.id, json.title, json.author, json.description, json.genre_id)
-      Genre.findById(book.genre_id).books.push(book)
-      const bookGenre = document.getElementById(id).getElementsByClassName("books")[0]
-      bookGenre.append(book.createBookDiv())
-      
-      if (bookGenre.children[0].className === 'book-div no-books') {
-        bookGenre.children[0].remove()
-      }
+      const genre = Genre.findById(book.genre_id)
+      genre.books().push(book)
+
+      const genreDiv = document.getElementById(genre.id)
+      genreDiv.innerHTML = ""
+
+      genre.appendChildren(genreDiv)
     })
-   
-    this.reset()
-    const bookForm = document.getElementById(`book-form-div-container-${id}`)
-    bookForm.className = "no-display"
   }
 
   static deleteBook() {
-    event.preventDefault()
-
+    event.preventDefault()  
+    
     fetch(`${BookAdapter.url}/${this.id}`, {
       method: "DELETE",
       headers: {
           "Content-Type": "application/json"
         } 
     }).then(() => { 
-      
-      const genreElement = document.getElementById(this.genre_id)
-
-      const booksElement = genreElement.getElementsByClassName("books")[0]
-      booksElement.innerHTML = ""
-
       const genre = this.genre()
-      const genreBooks = this.genre().books
-      const updateGenreBooks = genreBooks.filter(book => book !== this)
+      Book.delete(this)
+      
+      const genreDiv = document.getElementById(genre.id)
+      genreDiv.innerHTML = ""
 
-      genre.books = updateGenreBooks
-
-      if (genre.hasBooks()) {
-        const bookDivs = genre.bookDivs()
-        bookDivs.forEach(book => booksElement.append(book))
-      } else {
-        const noBooks = document.createElement("div")
-        noBooks.className = "book-div no-books"
-        noBooks.innerHTML = `${genre.name} genre empty.`
-        booksElement.append(noBooks)
-      }
+      genre.appendChildren(genreDiv)
     }) 
   }  
 }
