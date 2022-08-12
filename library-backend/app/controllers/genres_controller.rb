141c84d3f2b590cfb46b26db64b190ea5e899aa3
Nick <nelom3@gmail.com>
2022-08-12 17:16:18 -0400

    Removes unused code and intstance variables.

--- library-backend/app/controllers/genres_controller.rb
+++ library-backend/app/controllers/genres_controller.rb
@@ -1,55 +1,33 @@
 class GenresController < ApplicationController
-  before_action :set_genre, only: [:show, :update, :destroy]
 
-  # GET /genres
   def index
-    @genres = Genre.all
-
-    # render json: @genres, include: {books}
-    render json: @genres, except: [:created_at, :updated_at], include: {books: {except: [:created_at, :updated_at]}}
-    # render json: @genres.to_json(include: {books: {only: [:title]}})
-    # render json: authors.to_json(include: { books: {only: [:title]}})
-
+    genres = Genre.all
+    render json: genres, except: [:created_at, :updated_at], include: {books: {except: [:created_at, :updated_at]}}
   end
 
-  # GET /genres/1
   def show
-    render json: @genre
+    genre = Genre.find(params[:id])
+    render json: genre
   end
 
-  # POST /genres
   def create
-    @genre = Genre.new(genre_params)
-
-    if @genre.save
-      render json: @genre, status: :created, location: @genre
+    genre = Genre.new(genre_params)
+   
+    if genre.save
+      render json: genre, except: [:created_at, :updated_at], status: :created, location: genre
     else
-      render json: @genre.errors, status: :unprocessable_entity
+      render json: genre.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /genres/1
-  def update
-    if @genre.update(genre_params)
-      render json: @genre
-    else
-      render json: @genre.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /genres/1
   def destroy
-    @genre.destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_genre
-      @genre = Genre.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def genre_params
       params.require(:genre).permit(:name)
     end
+    
 end
